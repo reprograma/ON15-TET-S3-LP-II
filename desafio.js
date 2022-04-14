8f8a09e53bc01635e6ba352132c14714869385bb
Lilit <devlilitbandeira@gmail.com>
2022-04-13 22:09:06 -0300

    conteudo da revisao de quarta 13.04.2022

--- desafio.js
+++ desafio.js
@@ -11,3 +11,53 @@ let clienteLilit = [
     {produto: 'Tênis casual preto', valor: 120.0}, 
     {produto: 'meia calça transparente', valor: 30.0}]
 
+function realizarCompra(comprasDaCliente) {
+    let valorTotal = 0 // variável com a soma de todos os valores dos produtos sem desconto
+    let descontoAplicado = 0 // variável com a soma de todos os descontos aplicados
+    let valorFinal = 0 // variável com a soma de todos os valores dos produtos com desconto
+    let quantidadeDeProdutos = comprasDaCliente.length
+
+    for(let item of comprasDaCliente) {
+        valorTotal += item.valor
+        if (item.valor >= 200.0) {
+            descontoAplicado += (item.valor * 0.5)
+            valorFinal += (item.valor * 0.5)
+        } else if (item.valor >= 100.0) {
+            descontoAplicado += (item.valor * 0.2)
+            valorFinal += (item.valor * 0.8)
+        } else if (item.valor >= 80.0) {
+            descontoAplicado += (item.valor * 0.1)
+            valorFinal += (item.valor * 0.9)
+        } else if (item.valor >= 50.0) {
+            descontoAplicado += (item.valor * 0.05)
+            valorFinal += (item.valor * 0.95)
+        } else {
+            valorFinal += item.valor
+        }
+    }
+
+    let dataDaCompra = new Date()
+    console.log(dataDaCompra)
+
+    let notaFiscal = {
+        "Valor Total": `R$${valorTotal.toFixed(2).replace('.', ',')}`,
+        "Desconto Aplicado": `R$${descontoAplicado.toFixed(2).replace('.', ',')}`,
+        "Valor Final": `R$${valorFinal.toFixed(2).replace('.', ',')}`,
+        "Data de Compra": dataDaCompra.toLocaleDateString('pt-BR', {
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric'
+        })
+    }
+
+    if(quantidadeDeProdutos > 10 || valorFinal > 800.0) {
+        return console.table({
+            ...notaFiscal,
+            "Bônus": 'Você ganhou um cupom de R$50,00 para a sua prõxima compra'
+        })
+    } else {
+        return console.table(notaFiscal)
+    }
+}
+
+realizarCompra(clienteLilit)
