25c90151732ac24f3821dda2469482794489f313
Carolinenoletto <carolinenoletto@gmail.com>
2022-04-15 21:45:37 -0300

    atividade 3

--- desafio.js
+++ desafio.js
@@ -10,3 +10,68 @@ let clienteLilit = [
     {produto: 'Salto agulha 39', valor: 250.0}, 
     {produto: 'Tênis casual preto', valor: 120.0}, 
     {produto: 'meia calça transparente', valor: 30.0}]
+
+let clienteCarol = [
+   {produto: 'Bolsa pequena', valor: 49.0}, 
+   {produto: 'jaqueta jeans', valor: 300.0},]
+
+let clienteGabriela = [
+    {produto: 'Salto agulha 39', valor: 250.0}, 
+    {produto: 'Meia calça transparente', valor: 30.0},]
+ 
+     
+
+function realizarCompra(arrayDeCompra) {
+   let valorTotal = 0
+   let descontoAplicado = 0
+   let valorFinal = 0
+   let quantidadedeProdutos = arrayDeCompra.length
+
+    for(let item of arrayDeCompra) {
+      valorTotal += item.valor
+     if (item.valor >=200.0) {
+        valorFinal += (item.valor * 0.5)
+        descontoAplicado += (item.valor * 0.5)
+     } else if (item.valor >=100.0) {
+        valorFinal += (item.valor * 0.2)
+        descontoAplicado += (item.valor * 0.8)
+     } else if (item.valor >= 80.0) {
+         descontoAplicado += (item.valor * 0.1)
+         valorTotal += (item.valor * 0.9)
+     } else if (item.valor >= 50.0) {
+         descontoAplicado += (item.valor * 0.05)
+         valorFinal += (item.valor * 0.95)
+     } else {
+
+        valorFinal += item.valor
+
+
+         
+
+     }
+
+      
+
+    }
+
+    let notaFiscal = {
+      "Valor Total": valorTotal,
+      "Desconto Aplicado": descontoAplicado,
+      "Valor final": valorFinal
+}
+
+if(quantidadedeProdutos > 10 || valorFinal > 800.){
+return console.log ({ 
+   
+    ...notaFiscal,
+    "bônus": 'você ganhou um cupom de R$50,00 na próxima compra'
+
+})
+}
+
+
+realizarCompra(clienteLilit)
+realizarCompra(clienteCarol)
+realizarCompra(clienteGabriela)}
+
+
